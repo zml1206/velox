6acc688b44466446fd6b17f82cebf2ee2da43838
zml1206 <zhuml1206@gmail.com>
2025-02-10 10:29:26 +0800

    Register re-usable Presto date_trunc functions for Spark

--- velox/functions/sparksql/registration/RegisterDatetime.cpp
+++ velox/functions/sparksql/registration/RegisterDatetime.cpp
@@ -57,6 +57,8 @@ void registerDatetimeFunctions(const std::string& prefix) {
   registerFunction<DateSubFunction, Date, Date, int8_t>({prefix + "date_sub"});
   registerFunction<DateSubFunction, Date, Date, int16_t>({prefix + "date_sub"});
   registerFunction<DateSubFunction, Date, Date, int32_t>({prefix + "date_sub"});
+  registerFunction<DateTruncFunction, Timestamp, Varchar, Timestamp>(
+      {prefix + "date_trunc"});
   registerFunction<DayFunction, int32_t, Date>(
       {prefix + "day", prefix + "dayofmonth"});
   registerFunction<DayOfYearFunction, int32_t, Date>(
