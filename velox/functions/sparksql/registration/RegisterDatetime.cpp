90630f036d3ff76e79fb5bdcbbf0deff177b0c97
zml1206 <zhuml1206@gmail.com>
2025-07-08 10:27:57 +0800

    feat: Add Spark timestampadd function

--- velox/functions/sparksql/registration/RegisterDatetime.cpp
+++ velox/functions/sparksql/registration/RegisterDatetime.cpp
@@ -101,6 +101,12 @@ void registerDatetimeFunctions(const std::string& prefix) {
       Varchar,
       Timestamp,
       Timestamp>({prefix + "timestampdiff"});
+  registerFunction<
+    TimestampAddFunction,
+    Timestamp,
+    Varchar,
+    int64_t,
+    Timestamp>({prefix + "timestampadd"});
 }
 
 } // namespace facebook::velox::functions::sparksql
